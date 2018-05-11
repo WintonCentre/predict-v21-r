557050d4bcfb031028b2393f07df41739c0aa890
Gustavo Zomer <gzomer@dhcp-172-24-141-202.eduroam.wireless.private.cam.ac.uk>
2018-05-11 10:43:39 +0100

    Fix name

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject predict-r-model "0.1.0"
-  :description "Predict R modal for breast cancer"
+  :description "Predict R model for breast cancer"
   :url "https://github.com/WintonCentre/predict-v21-r"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
